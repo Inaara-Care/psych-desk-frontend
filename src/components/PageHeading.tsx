a789b86537a1a4a6dff0162aa9e6a4728da2405c
Sunny-64 <sunny6464n@gmail.com>
2025-03-27 19:24:27 +0530

    fix page heading component

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -3,10 +3,10 @@ import { useSidebarContext } from '@/context/Sidebar.context';
 import React from 'react';
 
 export const PageHeading = () => {
-  const { activeTab } = useSidebarContext();
+  const { tabHeading } = useSidebarContext();
   return (
     <div className="gap-4 mb-6">
-      <h1 className="text-2xl font-semibold mb-5">{activeTab}</h1>
+      <h1 className="text-2xl font-semibold mb-5">{tabHeading}</h1>
     </div>
   );
 };
--- src/context/Sidebar.context.tsx
+++ src/context/Sidebar.context.tsx
@@ -2,25 +2,38 @@
 
 import { createContext, useContext, useState, ReactNode } from "react";
 
-type SidebarContextType = {
+export type SidebarContextType = {
   activeTab: string;
+  tabHeading: string;
   setActiveTab: (tab: string) => void;
 };
 
 const initialState: SidebarContextType = {
-  activeTab: 'dashboard',
+  activeTab: '/',
+  tabHeading: 'Dashboard',
   setActiveTab: () => {}
 };
+
+const TAB_ALIAS: Record<string, string> = {
+  '': 'Dashboard',
+  'event-types': 'Event types',
+  'clients': 'Clients',
+  'contacts': 'Contacts',
+  'resources': 'Resources',
+  'settings': 'Settings',
+  'invoices': 'Invoices',
+  'calendar': 'Calendar',
+}
 
 const SidebarContext = createContext<SidebarContextType>(initialState);
 
 export const useSidebarContext = () => useContext(SidebarContext);
 
 export const SidebarProvider = ({ children }: { children: ReactNode }) => {
-  const [activeTab, setActiveTab] = useState<string>('dashboard');
+  const [activeTab, setActiveTab] = useState<string>('');
 
   return (
-    <SidebarContext.Provider value={{ activeTab, setActiveTab }}>
+    <SidebarContext.Provider value={{ tabHeading: TAB_ALIAS[activeTab], activeTab, setActiveTab }}>
       {children}
     </SidebarContext.Provider>
   );

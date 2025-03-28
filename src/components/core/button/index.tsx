192fb4f4b0e1984783509090e5b464e775256370
Hitesh Choudhary <hiteshchoudhary2109@gmail.com>
2025-03-28 08:27:18 +0530

    merged button

--- src/components/core/button/index.tsx
+++ src/components/core/button/index.tsx
@@ -1,50 +1,42 @@
 interface IButton {
-    label: string;
-    size: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-    variant: 'primary' | 'secondary' | 'outline' | 'link';
-    disabled?: boolean;
-    prefixIcon?: React.ReactNode;
-    suffixIcon?: React.ReactNode;
-    onClick?: () => void;
-    className?: string;
+  label: string;
+  size: 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'full';
+  variant: 'primary' | 'secondary' | 'outline' | 'link';
+  disabled?: boolean;
+  prefixIcon?: React.ReactNode;
+  suffixIcon?: React.ReactNode;
+  onClick?: () => void;
+  customClasses?: string;
 }
 
-const sizeUtil: Record<IButton["size"], string> = {
-    sm: "rounded-lg px-4 py-2 text-sm",
-    md: "rounded-lg px-5 py-3 text-base",
-    lg: "rounded-lg px-6 py-3 text-lg",
-    xl: "rounded-lg px-7 py-4 text-xl",
-    "2xl": "rounded-lg px-8 py-5 text-2xl",
+const sizeUtil: Record<string, string> = {
+  sm: 'rounded-lg px-4 py-2 text-sm',
+  full: 'w-full px-4 py-2'
 };
 
-const variantUtil: Record<IButton["variant"], string> = {
-    primary: "bg-blue-500 text-white hover:bg-blue-600",
-    secondary: "bg-gray-500 text-white hover:bg-gray-600",
-    outline: "border border-gray-500 text-gray-500 hover:bg-gray-100",
-    link: "text-blue-500 underline hover:text-blue-600",
+const variantUtil: Record<string, string> = {
+  primary: 'bg-primary text-white',
+  outline: 'border-1 border-gray-400',
+  link: 'text-primary',
 };
 
-const Button = ({
-    label,
-    size = "md",
-    variant = "primary",
-    disabled = false,
-    prefixIcon,
-    suffixIcon,
-    onClick,
-    className = "",
-}: IButton) => {
-    return (
-        <button
-            disabled={disabled}
-            onClick={onClick}
-            className={`font-semibold hover:cursor-pointer ${sizeUtil[size]} ${variantUtil[variant]} ${className}`}
-        >
-            {prefixIcon && <span>{prefixIcon}</span>}
-            <span>{label}</span>
-            {suffixIcon && <span>{suffixIcon}</span>}
-        </button>
-    );
+export const Button: React.FC<IButton> = ({
+  label,
+  size = 'md',
+  variant = 'primary',
+  disabled = false,
+  prefixIcon,
+  suffixIcon,
+  customClasses,
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      className={`font-semibold hover:cursor-pointer ${sizeUtil[size]} ${variantUtil[variant]} ${customClasses}`}
+    >
+      {prefixIcon && <span>{prefixIcon}</span>}
+      <span>{label}</span>
+      {suffixIcon && <span>{suffixIcon}</span>}
+    </button>
+  );
 };
-
-export default Button;
